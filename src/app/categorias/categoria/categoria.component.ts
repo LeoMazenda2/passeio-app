c9b4b5fe1b8c06570b0533ca8542de813878dbd9
lmazenda <leonildo.mazenda@xdsoftware.ao>
2025-05-16 10:04:30 +0100

    Projecto a cadastrar categorias

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
+import { CategoriaService } from '../categoria.service';
 @Component({
   selector: 'app-categoria',
   standalone: false,
@@ -9,7 +10,7 @@ import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 export class CategoriaComponent {
   camposForm: FormGroup;
 
-  constructor (){
+  constructor (private service : CategoriaService){
     this.camposForm = new FormGroup ({
         nome: new FormControl('', Validators.required),
         descricao: new FormControl('', Validators.required),
@@ -20,7 +21,15 @@ export class CategoriaComponent {
     this.camposForm.markAllAsTouched();
 
     if(this.camposForm.valid) {
-      console.table(this.camposForm.value);      
+      this.service
+       .salvar(this.camposForm.value)
+       .subscribe({
+        next: categoria => {
+          console.log('Categoria salva com sucesso ', categoria);
+          this.camposForm.reset();
+        },
+        error: erro => console.error('Ocorreu um erro ao salvar! ', erro)
+      });      
     }   
   }
 
