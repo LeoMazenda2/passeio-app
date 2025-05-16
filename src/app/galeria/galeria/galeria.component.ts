07ad6cb1af4e5229cd5dd60f8550f29732b43ef1
lmazenda <leonildo.mazenda@xdsoftware.ao>
2025-05-16 15:30:09 +0100

    a listar os lugares na galeria

--- src/app/galeria/galeria/galeria.component.ts
+++ src/app/galeria/galeria/galeria.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Categoria } from './../../categorias/categoria';
+import { Component, OnInit } from '@angular/core';
+import { Lugar } from '../../lugares/lugar';
+import { LugarService } from '../../lugares/lugar.service';
+import { CategoriaService } from '../../categorias/categoria.service';
 
 @Component({
   selector: 'app-galeria',
   standalone: false,
   templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.scss'
+  styleUrl: './galeria.component.scss',
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
+  lugares: Lugar[] = [];
+  categoriaFiltro: Categoria[] = [];
 
+  constructor(
+    private lugarService: LugarService,
+    private categoriaService: CategoriaService
+  ) {}
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas()
+      .subscribe(c => (this.categoriaFiltro = c));
+
+      this.lugarService.onterTodos()
+      .subscribe(l => (this.lugares = l));
+  }
+
+  getTotalEstrelas(lugar: Lugar) : string{
+    return '&#9733;'.repeat(lugar.avaliacao || 0) + '&#9734;'.repeat(5 - (lugar.avaliacao || 0))
+
+  }
 }
